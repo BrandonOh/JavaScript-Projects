7ac1a16bdc52dbff9bd847838fb35b963aab57b5
Brandon Oh <brandonoh2938@gmail.com>
2022-02-12 23:38:45 -0500

    created "on mouse over" function with text on html page.

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,18 +2,22 @@ var A = '"The quick brown fox jumps over the lazy dog"', //create variable A and
   B = " is an English-language pangram";                //create variable B and assign string text
 document.write(A); //Write out variable A on screen
 
-document.write("<br>") //break to next line
+document.write("<br>"); //break to next line
 
 window.alert(B); //Have a pop up of the variable B
 
 document.write(A + B); //concatenate variable A and B
 
-document.write("<br>") //break to next line
+document.write("<br>"); //break to next line
 
 document.write(A.fontcolor("green")); //write variable A in green text
 
 var C = B + A; //expression to concatenate two variables
 
-document.write("<br>") //break to next line
+document.write("<br>"); //break to next line
 
 document.write(C);
+
+function changeColor(){
+    document.getElementById("text").innerHTML = "I am Blue".fontcolor("blue");    
+}
