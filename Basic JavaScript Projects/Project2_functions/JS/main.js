bb41b0b91304b4f4aaa1c988123e0b3f074f1b82
Brandon Oh <brandonoh2938@gmail.com>
2022-02-14 22:11:14 -0500

    created a concatenation function.

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function add(){
+    var c = "cats "; //create a string variable.
+    c += " are furry." //concatenate the variable with a string.
+    document.getElementById("text").innerHTML = c; //replace the text in the element id.
+}
