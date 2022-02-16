d61bde9818460fc00fcc634427c2f4f1150f2eda
Brandon Oh <brandonoh2938@gmail.com>
2022-02-16 12:50:48 -0500

    Made multiple buttons to different math functions.

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,11 +1,53 @@
-var a = 8, b = 7, c = 0;
+var a = 25,
+  b = 7,
+  c = 0;
 
-function addition(){
-    c = a + b;
-    document.getElementById("Math").innerHTML = c;
+function addition() {
+  c = a + b;
+  document.getElementById("Math").innerHTML = c;
 }
 
-function subtraction(){
-    c = a - b;
+function subtraction() {
+  c = a - b;
+  document.getElementById("Math").innerHTML = c;
+}
+
+function divide() {
+  c = a / b;
+  document.getElementById("Math").innerHTML = c;
+}
+
+function multiply() {
+  c = a * b;
+  document.getElementById("Math").innerHTML = c;
+}
+
+function modulus() {
+  c = a % b;
+  document.getElementById("Math").innerHTML = c;
+}
+
+function lotsMath() {
+  c = (a * b + b) / a - b;
+  document.getElementById("Math").innerHTML = c;
+}
+
+function negate() {
+  c = a;
+  document.getElementById("Math").innerHTML = -c;
+}
+
+function increment() {
+  c++;
+  document.getElementById("Math").innerHTML = c;
+}
+
+function decrement() {
+  c--;
+  document.getElementById("Math").innerHTML = c;
+}
+
+function randomNum(){
+    c = Math.round((Math.random() * 100));
     document.getElementById("Math").innerHTML = c;
 }
