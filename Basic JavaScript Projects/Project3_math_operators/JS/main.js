248417cce482dce47e1f991236ca99729e0139e0
Brandon Oh <brandonoh2938@gmail.com>
2022-02-16 12:27:52 -0500

    created add and subtraction function.

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,11 @@
+var a = 8, b = 7, c = 0;
+
+function addition(){
+    c = a + b;
+    document.getElementById("Math").innerHTML = c;
+}
+
+function subtraction(){
+    c = a - b;
+    document.getElementById("Math").innerHTML = c;
+}
