56fde32ae7f787361744f8a8adc491abed512085
Brandon Oh <brandonoh2938@gmail.com>
2022-02-16 12:55:36 -0500

    added comments to js file.

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -2,52 +2,52 @@ var a = 25,
   b = 7,
   c = 0;
 
-function addition() {
+function addition() { // Add a and b variable and assign to c variable
   c = a + b;
   document.getElementById("Math").innerHTML = c;
 }
 
-function subtraction() {
+function subtraction() { // Subtracts a and b variable and assign to c variable
   c = a - b;
   document.getElementById("Math").innerHTML = c;
 }
 
-function divide() {
+function divide() { // Divides a and b variable and assign to c variable
   c = a / b;
   document.getElementById("Math").innerHTML = c;
 }
 
-function multiply() {
+function multiply() { // Multiplies a and b variable and assign to c variable
   c = a * b;
   document.getElementById("Math").innerHTML = c;
 }
 
-function modulus() {
+function modulus() { // Gets remainder of a divided into b variable and assign to c variable
   c = a % b;
   document.getElementById("Math").innerHTML = c;
 }
 
-function lotsMath() {
+function lotsMath() { // Does a long operation that includes add,subtract,divide and multiply
   c = (a * b + b) / a - b;
   document.getElementById("Math").innerHTML = c;
 }
 
-function negate() {
+function negate() { // Negates a and assign to c variable
   c = a;
   document.getElementById("Math").innerHTML = -c;
 }
 
-function increment() {
+function increment() { // Increments the c variable by 1
   c++;
   document.getElementById("Math").innerHTML = c;
 }
 
-function decrement() {
+function decrement() { // Decrements the c variable by 1
   c--;
   document.getElementById("Math").innerHTML = c;
 }
 
-function randomNum(){
+function randomNum(){ // Gets a random integer between 1 and 100, rounds that number and then assigns to c variable
     c = Math.round((Math.random() * 100));
     document.getElementById("Math").innerHTML = c;
 }
