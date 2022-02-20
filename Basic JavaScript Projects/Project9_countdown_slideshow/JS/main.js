f90002505166e105f3df27aaebe625f0d414379e
Brandon Oh <brandonoh2938@gmail.com>
2022-02-20 15:59:59 -0500

    re-added countdown function and element, fixed slideshow display with defer.

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -33,3 +33,19 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block"; //display the current index
   dots[slideIndex - 1].className += " active"; //set the dot as active based on what index we are on.
 }
+
+function countdown() {
+  var seconds = document.getElementById("seconds").value;//set value of input to variable seconds
+
+  function tick() {//function to count down from variable seconds to 0 and display it in paragraph element.
+    seconds = seconds -1;
+    document.getElementById("timer").innerHTML = seconds;
+    var time = setTimeout(tick, 1000);
+    if( seconds == -1){
+      alert("Time's up");
+      clearTimeout(time);
+      document.getElementById("timer").innerHTML = "";
+    }
+  }
+  tick()
+}
