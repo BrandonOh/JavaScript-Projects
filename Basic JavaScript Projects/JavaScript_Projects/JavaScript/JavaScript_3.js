3a581266499e007373de15b9bbc2bd1e51c8895b
Brandon Oh <brandonoh2938@gmail.com>
2022-02-21 19:57:08 -0500

    Fixed type in html_1 file and created html_3 file to test out data attributes.

---
+++ Basic JavaScript Projects/JavaScript_Projects/JavaScript/JavaScript_3.js
@@ -0,0 +1,4 @@
+function displayType(alignment) {
+    var characterAlign = alignment.getAttribute("data-alignment-type");
+    alert(alignment.innerHTML + " has a " + characterAlign + " alignment.")
+}
